644abc82aa146ef85f1a633869c56ce3716f4d58
Debora MR <dmedina@iesrafaelalberti.es>
2024-02-26 21:29:38 +0100

    feat: se muestran los proyectos y las paletas de color

--- src/pages/Proyectos.vue
+++ src/pages/Proyectos.vue
@@ -1,6 +1,7 @@
 <script setup>
 import {computed, onMounted} from "vue";
 import {useProjectStore} from '../stores/ProjectStore';
+import Alert from "../shared/Alert.vue";
 
 const projectStore = useProjectStore();
 
@@ -19,10 +20,26 @@ const projectData = computed(() => {
   <section class="container__center">
     <h2>Proyectos</h2>
 
+    <!--itera proyectos-->
     <div v-for="project in projectData.projects" :key="project.id">
       <h3>{{ project.name }}</h3>
       <p>{{ project.description }}</p>
       <p>Creado en: {{ project.created_at }}</p>
+
+      <!--itera paletas-->
+      <ul v-for="palette in project.palettes" :key="palette.id">
+
+        <!--itera colores de paletas-->
+        <li class="main__article__li" v-for="color in palette.colors" :key="color.id"
+            :style="{
+              backgroundColor: '#'+color.hex,
+              width: 47 + index + '%',
+              height: 14 + index + '%',
+           }">
+          {{ color.name }}
+        </li>
+      </ul>
+      <alert></alert>
     </div>
 
   </section>
