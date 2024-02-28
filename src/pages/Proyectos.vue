e2b6ed9fa3614a25ca27fe4147458ae01c9b8c30
Debora MR <dmedina@iesrafaelalberti.es>
2024-02-28 20:12:28 +0100

    Feat: Crear y Editar Proyectos

--- src/pages/Proyectos.vue
+++ src/pages/Proyectos.vue
@@ -2,6 +2,7 @@
 import {computed, onMounted} from "vue";
 import {useProjectStore} from '../stores/ProjectStore.js';
 import Alert from "../shared/Alert.vue";
+import Modal from "../shared/Modal.vue";
 
 const projectStore = useProjectStore();
 
@@ -16,32 +17,124 @@ const projectData = computed(() => {
 });
 </script>
 
+<script>
+import {useUserStore} from "../stores/UserStore.js";
+import {useProjectStore} from '../stores/ProjectStore.js';
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      newProjectModal: false,
+      editProjectModal: false,
+      project_name: '',
+      project_description: '',
+      project_id: ''
+    }
+  },
+  methods: {
+    showNewProject() {
+      this.project_name = '';
+      this.project_description = '';
+      this.newProjectModal = true;
+    },
+    showEditProject(project) {
+      this.project_id = project.id;
+      this.project_name = project.name;
+      this.project_description = project.description;
+      this.editProjectModal = true;
+    },
+    closeModal() {
+      this.newProjectModal = false;
+      this.editProjectModal = false;
+    },
+    async createNewProject() {
+        try {
+          const config = {
+            headers: {Authorization: `Bearer ${useUserStore().token}`}
+          };
+          const response = await axios.post('/api/v1/projects', {
+            name: this.project_name,
+            description: this.project_description
+          }, config);
+          await useProjectStore().getProjects();
+          this.closeModal()
+
+          console.log(response.data); // Hacer algo con la respuesta del servidor si es necesario
+        } catch (error) {
+          console.error('Error al crear proyecto:', error);
+        }
+    },
+    async editProject() {
+      try {
+        const config = {
+          headers: {Authorization: `Bearer ${useUserStore().token}`}
+        };
+        const response = await axios.put('/api/v1/projects/' + this.project_id, {
+          name: this.project_name,
+          description: this.project_description
+        }, config);
+        await useProjectStore().getProjects();
+        this.closeModal()
+
+        console.log(response.data); // Hacer algo con la respuesta del servidor si es necesario
+      } catch (error) {
+        console.error('Error al crear proyecto:', error);
+      }
+    }
+  }
+}
+</script>
+
 <template>
   <section class="container__center">
     <h2>Proyectos</h2>
 
-    <!--itera proyectos-->
-    <div v-for="project in projectData.projects" :key="project.id">
-      <div v-if="project.editing">
-        <form @submit.prevent="projectStore.editProjects(project.id, project.name, project.description)">
-          <label for="name">Nombre:</label>
-          <input id="name" v-model="project.name" type="text" />
+    <button class="button" @click="showNewProject">Nuevo Proyecto</button>
 
-          <label for="description">Descripción:</label>
-          <textarea id="description" v-model="project.description"></textarea>
-
-          <button type="submit" >Guardar</button>
-          <button type="button" @click="project.editing = false">Cancelar</button>
+    <!-- Modal para la Creacion de Proyectos -->
+    <modal :isOpen="newProjectModal" @modal-close="closeModal" name="first-modal">
+      <template #header><h2>Nuevo Proyecto_</h2></template>
+      <template #content><p>Crear nuevo</p>
+        <form @submit.prevent="createNewProject">
+          <fieldset class="section__article">
+            <label>Nombre del Proyecto:</label><br>
+            <input v-model="project_name" type="text" id="project_name" name="project_name" required><br>
+            <label>Descripción:</label><br>
+            <textarea v-model="project_description" id="project_description" name="project_description" rows="5" cols="50" required></textarea>
+            <br>
+            <button class="button" type="submit">Nuevo Proyecto</button>
+          </fieldset>
         </form>
-      </div>
-      <div v-else>
-        <button @click="project.editing = true"><font-awesome-icon icon="fa-solid fa-pen-to-square" class="icon"/></button>
+      </template>
+    </modal>
+
+    <!-- Modal para la Edicion de Proyectos -->
+    <modal :isOpen="editProjectModal" @modal-close="closeModal" name="first-modal">
+      <template #header><h2>Editar Proyecto_</h2></template>
+      <template #content><p>Cambia los datos del proyecto</p>
+        <form @submit.prevent="editProject()">
+          <fieldset class="section__article">
+            <label>Nombre del Proyecto:</label><br>
+            <input v-model="project_name" type="text" id="project_name" name="project_name" required><br>
+            <label>Descripción:</label><br>
+            <textarea v-model="project_description" id="project_description" name="project_description" rows="5" cols="50" required></textarea>
+            <br>
+            <button class="button" type="submit">Editar Proyecto</button>
+          </fieldset>
+        </form>
+      </template>
+    </modal>
+
+    <!--itera proyectos-->
+    <div v-for="project in projectData.projects" :key="project.id" class="card container_project">
+
+        <button @click="showEditProject(project)"><font-awesome-icon icon="fa-solid fa-pen-to-square" class="icon"/></button>
         <button @click="projectStore.deleteProjects(project.id)"><font-awesome-icon icon="fa-solid fa-xmark" class="icon"/></button>
 
         <h3>{{ project.name }}</h3>
         <p>{{ project.description }}</p>
         <p>Creado en: {{ project.created_at }}</p>
-
         <!--itera paletas-->
         <ul v-for="palette in project.palettes" :key="palette.id">
 
@@ -56,7 +149,6 @@ const projectData = computed(() => {
           </li>
         </ul>
         <alert></alert>
-      </div>
     </div>
 
   </section>
@@ -77,5 +169,21 @@ button {
   -webkit-appearance: button; /* for input */
   -moz-user-select: none;
   -ms-user-select: none;
+}
+
+.card {
+  /* Add shadows to create the "card" effect */
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+}
+
+/* On mouse-over, add a deeper shadow */
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+/* Add some padding inside the card container */
+.container_project {
+  padding: 2px 16px;
 }
 </style>
