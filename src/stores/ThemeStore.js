09f63987c2f2625f76d08b927a813e88de77fe96
Debora MR <dmedina@iesrafaelalberti.es>
2024-03-01 19:36:22 +0100

    Fix: Hacer el tema como persistente

--- src/stores/ThemeStore.js
+++ src/stores/ThemeStore.js
@@ -3,11 +3,12 @@ import { defineStore } from 'pinia';
 export const useThemeStore = defineStore({
     id: 'theme',
     state: () => ({
-        theme: 'light', // valor por defecto
+        theme: localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light', // valor por defecto
     }),
     actions: {
         toggleTheme() {
             this.theme = this.theme === 'light' ? 'dark' : 'light';
+            localStorage.setItem('theme', this.theme);
             document.documentElement.setAttribute('data-theme', this.theme);
         },
     },
